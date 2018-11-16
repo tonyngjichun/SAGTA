8494227417f6104dfdbdf56d0bf9cce9a8f27159
tonyngjichun <tonyngjichun@gmail.com>
2018-11-16 19:01:30 +0000

    fixed nargs in main

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import os
 import numpy as np
 import argparse
 
-def main(config):
+def main(args):
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--file_path', type=str, default='../AllAssignments',required=True)
-    parser.add_argument('--num_GTA', type=int, default=4, required=True)
+    parser.add_argument("-n",'--num_GTA', type=int, default=4, required=True)
     parser.add_argument('--cross_checking', type=bool, default=True, required=True)
-    parser.add_argument('--split_evenly', type=bool, default=True, help='Split the courseworks evenly across all n = num_GTA GTAs. Default is True. If false, please enter field "split_proportion".')     
-    parser.add_argument('--split_proportion', type=, default=None, help='Proportion of splitting work among n = num_GTAs. Leave as none if argument "--split_evenly  True".')        
-    parser.add_argument('-- ')
-    parser.add_argument('-- ')
+    parser.add_argument('--split_evenly', type=bool, default=True, help='Split the courseworks evenly across all n GTAs. Default is True. If false, please enter field "split_proportion".')     
+    parser.add_argument('--split_proportion', type=float, nargs='+', default=None, help='Proportion of splitting work among n GTAs. Leave as none if argument "--split_evenly  True".')        
+    parser.add_argument('--GTA_names', type=str, nargs='+', default=[], help='Names of GTAs in str arr form. Length of string array should be equal to num_GTA. If not provided ["1"... "n"] would be automatically assigned',required=False)
+    parser.add_argument('--pdf_only', type=bool, default=True, help='Toggle if file loader only reads files ending with .pdf')
     parser.add_argument('-- ')
     parser.add_argument('-- ')
 
-    config = parser.parse_args()
-    main(config)
+    args = parser.parse_args()
+    main(args)

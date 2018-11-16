70b7229a0f2e6bb22a6e08e3b9f9510f34bfb9cd
tonyngjichun <tonyngjichun@gmail.com>
2018-11-16 19:12:49 +0000

    '-n' arg in argpase fix

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--file_path', type=str, default='../AllAssignments',required=True)
-    parser.add_argument("-n",'--num_GTA', type=int, default=4, required=True)
+    parser.add_argument('-n', '--num_GTA', type=int, default=4, required=True)
     parser.add_argument('--cross_checking', type=bool, default=True, required=True)
     parser.add_argument('--split_evenly', type=bool, default=True, help='Split the courseworks evenly across all n GTAs. Default is True. If false, please enter field "split_proportion".')     
     parser.add_argument('--split_proportion', type=float, nargs='+', default=None, help='Proportion of splitting work among n GTAs. Leave as none if argument "--split_evenly  True".')        

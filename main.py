e943ab0e174f868cd5d48b2f8cae595866820a9d
tonyngjichun <tonyngjichun@gmail.com>
2018-11-16 14:26:21 +0000

    hello git1

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import numpy as np
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file_path', type=str, default='../AllAssignments')
+    parser.add_argument('--numGTA', type=int, default=4)
+    parser.add_argument('--')
+    parser.add_argument('-- ')     
+    parser.add_argument('-- ')        
+    parser.add_argument('-- ')
+    parser.add_argument('-- ')
+    parser.add_argument('-- ')
+    parser.add_argument('-- ')
+
+    config = parser.parse_args()

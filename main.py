0147631d6c77460e00f370db06cf2aaa4ac8a80a
tonyngjichun <tonyngjichun@gmail.com>
2018-11-16 15:41:25 +0000

    Commit for Macbook to clone

--- main.py
+++ main.py
@@ -2,16 +2,20 @@ import os
 import numpy as np
 import argparse
 
+def main(config):
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--file_path', type=str, default='../AllAssignments')
-    parser.add_argument('--numGTA', type=int, default=4)
-    parser.add_argument('--')
-    parser.add_argument('-- ')     
-    parser.add_argument('-- ')        
+    parser.add_argument('--file_path', type=str, default='../AllAssignments',required=True)
+    parser.add_argument('--num_GTA', type=int, default=4, required=True)
+    parser.add_argument('--cross_checking', type=bool, default=True, required=True)
+    parser.add_argument('--split_evenly', type=bool, default=True, help='Split the courseworks evenly across all n = num_GTA GTAs. Default is True. If false, please enter field "split_proportion".')     
+    parser.add_argument('--split_proportion', type=, default=None, help='Proportion of splitting work among n = num_GTAs. Leave as none if argument "--split_evenly  True".', )        
     parser.add_argument('-- ')
     parser.add_argument('-- ')
     parser.add_argument('-- ')
-    parser.add_argument('-- ') assd 
+    parser.add_argument('-- ')
 
     config = parser.parse_args()
+    main(config)
